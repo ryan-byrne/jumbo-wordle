0a3549171aaf1030074dcbf4b69fdbd16450cb34
Ryan <ryan-byrne@users.noreply.github.com>
2022-12-19 11:19:29 -0500

    fix typo

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN npm run build
 COPY package*.json ./
 RUN npm ci
 COPY . .
-CMD [ "node", "index.js"]
+CMD [ "node", "app.js"]
--- app.js
+++ app.js
@@ -29,5 +29,4 @@ app.get('*', (req, res, next) =>
 app.listen(PORT, (error) => {
   if (!error) console.log("Server is Successfully Running, and App is listening on port "+ PORT)
   else console.log("Error occurred, server can't start", error);
-}
-);
+});
